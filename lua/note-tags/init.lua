b6530f2b16087e1f19b0febac03870270abcae06
Adam Druzd <adam.druzd@uni.lu>
2023-04-14 23:21:07 +0200

    WIP getting close

--- lua/note-tags/init.lua
+++ lua/note-tags/init.lua
@@ -1,8 +1,12 @@
 local telescope = require('telescope')
 local actions = require('telescope.actions')
+local action_state = require('telescope.actions.state')
 local pickers = require('telescope.pickers')
+local sorters = require('telescope.sorters')
 local finders = require('telescope.finders')
+local previewers = require('telescope.previewers')
 local built = require('telescope.builtin')
+local utils = require('telescope.utils')
 local conf = require('telescope.config').values
 
 local M = {}
@@ -11,80 +15,119 @@ local function get_notes_folder()
   if M.notes_folder ~= nil then
     return M.notes_folder
   end
-  return vim.fn.getcwd()
+  return "~/Notes"
+  -- return vim.fn.getcwd()
 end
 
-local function read_tags()
+-- This method will check in configuration if user has own tags seperator
+-- if not it will return mine <!--tags-->
+local function get_tags_separator()
+  return "<!--tags-->"
+end
+
+-- This method will check configuration and return method of reading tags
+-- there are two methos: separator or wholeFile
+-- * **separator** is a method that reads tags after separator (should be at the end of file)
+-- * **wholeFile** reads file and return tags matching regular expression
+local function get_read_tag_method()
+  return "separator"
+  -- return "wholeFile"
+end
+
+-- this methos will return
+local function get_notes_with_separator()
+  local search = get_tags_separator()
   local notes_folder = get_notes_folder()
-  local tag_set = {}
-  for _, file in ipairs(vim.fn.glob(notes_folder .. "/*.md", true, true)) do
-    local file_tags = vim.fn.matchlist(vim.fn.readfile(file), "\\v#([^ ]+)", 0)
-    for _, tag in ipairs(file_tags) do
-      if tag ~= "" then
-        tag_set[tag] = true
-      end
+  local grep_command = "grep -r -l '" .. search .. "' " .. notes_folder .. "/*.md | sort | uniq"
+  local grep_result = vim.fn.systemlist(grep_command)
+  local filenames = {}
+  for _, result in ipairs(grep_result) do
+    if not vim.tbl_contains(filenames, result) then
+      table.insert(filenames, result)
     end
   end
-  return vim.tbl_keys(tag_set)
+  return filenames
 end
 
-function M.find_files_for_tag(tag)
-  local notes_folder = get_notes_folder()
-  local files = vim.fn.glob(notes_folder .. "/**/*.md", true, true)
+-- Reads tags only after the separator
+local function read_tags_after_separator()
+  local files = get_notes_with_separator()
+  local separator = get_tags_separator()
+  separator = string.gsub(separator, "-", "%%-")
   local tags = {}
-
   for _, file in ipairs(files) do
-    local contents = io.open(file):read("*all")
-    if contents:find("#" .. tag, 1, true) then
-      table.insert(tags, { path = file, filename = string.match(file, "[^/]+$"), lnum = 1 })
-    end
-  end
-
-  if #tags > 0 then
-    print(vim.inspect(tags))
-
-    local opts = {
-      prompt_title = "Note for tag #" .. tag,
-      finder = require("telescope.finders").new_table {
-        results = tags,
-        entry_maker = function(entry)
-          return {
-            value = entry.path,
-            display = entry.filename,
-            ordinal = entry.path,
-          }
-        end,
-      },
-      sorter = require("telescope.sorters").get_generic_fuzzy_sorter(),
-      attach_mappings = function(_, map)
-        map("i", "<CR>", function(prompt_bufnr)
-          local selection = require("telescope.actions.state").get_selected_entry(prompt_bufnr)
-          require("telescope.actions").close(prompt_bufnr)
-          vim.api.nvim_command("edit " .. selection.value)
-        end)
-        return true
-      end,
-    }
-    require("telescope.pickers").new(opts):find()
-  else
-    print("No files found for tag " .. tag)
-  end
-end
-
-local function find_notes_for_tag(tag)
-  local notes_folder = get_notes_folder()
-  local notes_list = {}
-  for _, file in ipairs(vim.fn.glob(notes_folder .. "/*.md", true, true)) do
-    local file_tags = vim.fn.matchlist(vim.fn.readfile(file), "\\v#([^ ]+)", 0)
-    for _, t in ipairs(file_tags) do
-      if t == tag then
-        table.insert(notes_list, file)
-        break
+    local lines = vim.fn.readfile(file)
+    local start = false
+    for _, line in ipairs(lines) do
+      if start then
+        for tag in string.gmatch(line, '#(%S+)') do
+          table.insert(tags, tag)
+        end
+      end
+      if string.match(line, separator) ~= nil then
+        start = true
       end
     end
   end
-  return notes_list
+  return tags
 end
+
+-- Reads tags in whole file using regularexpression
+local function read_tags_whole_file()
+  print('Whole file')
+  local notes_folder = get_notes_folder()
+  local search = "#[^ #=]+"
+  local grep_command = "grep -r -E '" .. search .. "' " .. notes_folder .. "/*.md | sort | uniq"
+  local grep_result = vim.fn.systemlist(grep_command)
+  local tags = {}
+  -- print(vim.inspect(grep_result))
+  for _, line in ipairs(grep_result) do
+    for tag in string.gmatch(line, '#([%w-]+)') do
+      if not vim.tbl_contains(tags, tag) then
+        table.insert(tags, tag)
+      end
+    end
+  end
+  return tags
+end
+
+-- Main method to read tags basically it choose right method for reading tags based on users preferences
+local function read_tags()
+  if get_read_tag_method() == "separator" then
+    return read_tags_after_separator()
+  else
+    return read_tags_whole_file()
+  end
+end
+
+
+local function display_tags(opts)
+  opts = opts or {}
+  local tags = opts.tags or {}
+
+  print(vim.inspect(tags))
+
+  local pickers_opts = {
+    prompt_title = "Search for tag",
+    finder = finders.new_table(tags),
+    sorter = sorters.get_generic_fuzzy_sorter({}),
+    previewer = previewers.cat.new(opts),
+    attach_mappings = function(prompt_bufnr, map)
+      actions.select_default:replace(
+        function()
+          actions.close(prompt_bufnr)
+          local selection = action_state.get_selected_entry()
+          -- vim.api.nvim_put({ "#" .. selection[1] .. " " }, "", false, true)
+        end
+      )
+      return true
+    end
+  }
+  -- #story #me
+  local tag_picker = pickers.new(pickers_opts)
+  tag_picker:find()
+end
+
 
 function M.tags()
   local tags = read_tags()
@@ -94,7 +137,7 @@ function M.tags()
   else
     print(table.concat(tags, ", "))
   end
-  require("telescope.builtin").quickfix({ entries = tags })
+  display_tags({ tags = tags })
 end
 
 function M.notes()
@@ -125,12 +168,13 @@ function M.setup()
     { noremap = true })
 end
 
-return setmetatable({}, {
-  __index = function(_, k)
-    if M[k] then
-      return M[k]
-    else
-      error("Invalid method " .. k)
-    end
-  end,
-})
+-- return setmetatable({}, {
+--   __index = function(_, k)
+--     if M[k] then
+--       return M[k]
+--     else
+--       error("Invalid method " .. k)
+--     end
+--   end,
+-- })
+M.tags()
