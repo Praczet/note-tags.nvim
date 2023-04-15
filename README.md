be5fc3f0fc0571e582443f92aec688cdf29e0d49
Adam Druzd <adam.druzd@uni.lu>
2023-04-16 00:27:25 +0200

    WIP List of tags works, notes with tags works, selecting tag works, addin tag to section works

--- README.md
+++ README.md
@@ -2,10 +2,15 @@
 
 THIS IS WIP and for now there is no point to install it :-) Still learning
 
+Some update it starts to look like plugin I wanted... It can read folder, it can read tags from files, it supports <!--tags--> section
+
 Ok. This is my second plugin. This time I will try to use telescope. The main purpose of it is to tags notes. On the bottom of notes I am putting tags such as `#sql #JSON` etc. This plugin should open Telescope read all tags from files in a folder[^1] and display list in telescope. After selecting tag it should show list of files having this tag.
 
 # ToDo
 
-Everything :-D
+- [ ] Add Mapping for control + O to open notes filtered by tag
+- [ ] Add reading data from config, which I have no Idea how
+- [ ] Add Mapping for leader n
+- [ ] Add it as extension of Telescope
 
 [^1]: Folder should be or declared in config or plugin will take current one.
--- lua/note-tags/init.lua
+++ lua/note-tags/init.lua
@@ -61,7 +61,9 @@ local function read_tags_after_separator()
     for _, line in ipairs(lines) do
       if start then
         for tag in string.gmatch(line, '#(%S+)') do
-          table.insert(tags, tag)
+          if not vim.tbl_contains(tags, tag) then
+            table.insert(tags, tag)
+          end
         end
       end
       if string.match(line, separator) ~= nil then
@@ -69,6 +71,7 @@ local function read_tags_after_separator()
       end
     end
   end
+  table.sort(tags)
   return tags
 end
 
@@ -88,6 +91,7 @@ local function read_tags_whole_file()
       end
     end
   end
+  table.sort(tags)
   return tags
 end
 
@@ -100,7 +104,6 @@ local function read_tags()
   end
 end
 
-
 local function display_tags(opts)
   opts = opts or {}
   local tags = opts.tags or {}
@@ -109,15 +112,25 @@ local function display_tags(opts)
 
   local pickers_opts = {
     prompt_title = "Search for tag",
+    results_title = "Tags",
     finder = finders.new_table(tags),
     sorter = sorters.get_generic_fuzzy_sorter({}),
-    previewer = previewers.cat.new(opts),
+    previewer = previewers.new_buffer_previewer({
+      title = "Notes",
+      width = 0.8,
+      define_preview = function(self, entry, status)
+        local notes = M.get_notes_for_tag(entry.value) or { "-- notes not found --" }
+        local abs_path = vim.fn.expand(get_notes_folder()) .. "/"
+        notes = M.table_map(notes, function(item) return string.gsub(item, abs_path, "") end)
+        vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, notes)
+      end
+    }),
     attach_mappings = function(prompt_bufnr, map)
       actions.select_default:replace(
         function()
           actions.close(prompt_bufnr)
           local selection = action_state.get_selected_entry()
-          -- vim.api.nvim_put({ "#" .. selection[1] .. " " }, "", false, true)
+          M.add_tag(selection[1])
         end
       )
       return true
@@ -128,6 +141,102 @@ local function display_tags(opts)
   tag_picker:find()
 end
 
+
+
+local function get_notes_for_tag_separator(tag)
+  local files = get_notes_with_separator()
+  local separator = get_tags_separator()
+  separator = string.gsub(separator, "-", "%%-")
+  local notes = {}
+  for _, file in ipairs(files) do
+    local lines = vim.fn.readfile(file)
+    local start = false
+    for _, line in ipairs(lines) do
+      if start then
+        for note in string.gmatch(line, '#' .. tag .. '') do
+          if not vim.tbl_contains(notes, file) then
+            table.insert(notes, file)
+          end
+        end
+      end
+      if string.match(line, separator) ~= nil then
+        start = true
+      end
+    end
+  end
+  return notes
+end
+
+
+local function get_notes_for_tag_whilefile(tag)
+  local notes_folder = get_notes_folder()
+  local grep_command = "grep -r  -l -E '#" .. tag .. "\\s|#" .. tag .. ",' " .. notes_folder .. "/*.md | sort | uniq "
+  local grep_result = vim.fn.systemlist(grep_command)
+  local notes = {}
+  -- print(vim.inspect(grep_result))
+  for _, line in ipairs(grep_result) do
+    for note in string.gmatch(line, '#([%w-]+)') do
+      if not vim.tbl_contains(notes, note) then
+        table.insert(notes, note)
+      end
+    end
+  end
+  return notes
+end
+
+function M.add_tag(tag)
+  local tags_found  = false
+  local bfnr        = vim.api.nvim_get_current_buf()
+  local lines       = vim.api.nvim_buf_line_count(bfnr)
+  local separator   = get_tags_separator()
+  local r_separator = string.gsub(separator, "-", "%%-")
+  -- vim.api.nvim_put({ "#" .. selection[1] .. " " }, "", false, true)
+  for i, line in ipairs(vim.fn.getbufline(bfnr, 1, "$")) do
+    if string.find(line, "^--" .. r_separator) then
+      tags_found = true
+      for j = i + 1, #vim.fn.getbufline(bfnr, i, "$") + i do
+        local current_line = vim.fn.getbufline(bfnr, j, j)[1]
+        if string.find(current_line, "#") then
+          if string.find(current_line, "#" .. tag .. "%s") or string.find(current_line, "#" .. tag .. "$") then
+            print('Tags exists')
+            return
+          else
+            vim.api.nvim_buf_set_lines(bfnr, j - 1, j, false, { current_line .. " #" .. tag })
+            print('adding at the end', j, current_line)
+            return
+          end
+        end
+      end
+      vim.api.nvim_buf_set_lines(bfnr, lines, lines, false, { "#" .. tag })
+      return
+    end
+  end
+
+  -- If the buffer does not contain a line with "<!--tags-->", add it to the end
+  if not tags_found then
+    vim.api.nvim_buf_set_lines(bfnr, lines, lines, false, { "---" })
+    vim.api.nvim_buf_set_lines(bfnr, lines + 1, lines + 1, false, { '' })
+    vim.api.nvim_buf_set_lines(bfnr, lines + 2, lines + 2, false, { separator })
+    vim.api.nvim_buf_set_lines(bfnr, lines + 3, lines + 3, false, { '' })
+    vim.api.nvim_buf_set_lines(bfnr, lines + 4, lines + 4, false, { "#" .. tag })
+  end
+end
+
+function M.table_map(tbl, fn)
+  local new_tbl = {}
+  for i, v in ipairs(tbl) do
+    new_tbl[i] = fn(v, i, tbl)
+  end
+  return new_tbl
+end
+
+function M.get_notes_for_tag(tag)
+  if get_read_tag_method() == "separator" then
+    return get_notes_for_tag_separator(tag)
+  else
+    return get_notes_for_tag_whilefile(tag)
+  end
+end
 
 function M.tags()
   local tags = read_tags()
@@ -168,13 +277,16 @@ function M.setup()
     { noremap = true })
 end
 
--- return setmetatable({}, {
---   __index = function(_, k)
---     if M[k] then
---       return M[k]
---     else
---       error("Invalid method " .. k)
---     end
---   end,
--- })
-M.tags()
+return setmetatable({}, {
+  __index = function(_, k)
+    if M[k] then
+      return M[k]
+    else
+      error("Invalid method " .. k)
+    end
+  end,
+})
+-- M.tags()
+--<!--tags-->
+
+--#dream #lion
